cb4bd49a83210b1c8dec1845afaaac1f7cfb67fe
niqzart <qwert45hi@yandex.ru>
2025-04-24 21:10:31 +0300

    feat: classic board generation

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,5 +1,19 @@
 extends Node2D
 
+const TILE_SCALE: float = 0.25
+
+@export var EmptyTileScene: PackedScene
+
+
+func _ready() -> void:
+    var background_tile: Node2D
+    for x_index in range(4):
+        for y_index in range(4):
+            background_tile = EmptyTileScene.instantiate()
+            background_tile.position = Vector2(5 + 105 * x_index, 5 + 105 * y_index)
+            background_tile.scale = Vector2(TILE_SCALE, TILE_SCALE)
+            $Background.add_child(background_tile)
+
 
 func _on_update_timer_timeout() -> void:
     var new_tile_power = $NumberTile.tile_power + 1
